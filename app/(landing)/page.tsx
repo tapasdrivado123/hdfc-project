19b0131a50cfd04b605b511a2ac65a07d0205235
tapasdrivado123 <techsupport@ezeerooms.com>
2026-02-03 16:11:56 +0530

    first commit

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <div>page</div>
+    </div>
+  );
+}
+
+export default page;
