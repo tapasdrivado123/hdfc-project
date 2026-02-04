34fe271e7f32dd7b40f3c3f3424958bb701a3cb2
tapasdrivado123 <techsupport@ezeerooms.com>
2026-02-04 16:15:56 +0530

    ADD ui and style WhyTravelSmarter and Faq

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,11 @@
+import Faq from "./components/Faq";
 import WhyTravelSmarter from "./components/WhyTravelSmarter";
 
 export default function page() {
   return (
     <div>
       <WhyTravelSmarter />
+      <Faq />
     </div>
   );
 }
